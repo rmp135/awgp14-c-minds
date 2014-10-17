319f48802f2f737ab3b36726929ea574e86ef715
Jack Hames <h028884a@student.staffs.ac.uk>
2014-10-17 16:29:13 +0100

    Added abilty to create a Service Manager

---
+++ CSharpMinds/Interfaces/IService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpMinds.Interfaces
+{
+    public interface IService
+    {
+        static readonly string name { get; }
+    }
+}
---
+++ CSharpMinds/Managers/ServiceManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSharpMinds.Interfaces;
+
+namespace CSharpMinds.Managers
+{
+    public class ServiceManager : IServiceManager
+    {
+        List<IService> services;
+        public List<IService> Services
+        {
+            get { return services; }
+        }
+
+        public ServiceManager()
+        {
+            services = new List<IService>();
+        }
+
+        public void AddService(IService service)
+        {
+            services.Add(service);
+        }
+
+        public void Remove(IService service)
+        {
+            services.Remove(service);
+        }
+    }
+}
