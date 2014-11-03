04520d8aa407560f39ed5888f4d2667b0f59dccf
Grant Siddall <s019365a@student.staffs.ac.uk>
2014-11-03 14:24:37 +0000

    Input interface partially implemented KeyBoard Input partially implemented

---
+++ CSharpMinds/Interfaces/IInput.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpMinds
+{
+    //Allows Components to react to different types of input device
+    public interface IInput
+    {
+        void readInput();
+    }
+}
---
+++ CSharpMinds/KeyBoardInput.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CSharpMinds.Interfaces
+{
+    class KeyBoardInput : IInput
+    {
+        ConsoleKeyInfo cki;
+
+        public void readInput()
+        {
+            cki = Console.ReadKey();
+            if (cki.Key != ConsoleKey.Escape)
+            {
+                //Do Stuff
+            }
+            else
+            {
+                Console.WriteLine("End Program"); 
+            }
+        }
+    }
+}
