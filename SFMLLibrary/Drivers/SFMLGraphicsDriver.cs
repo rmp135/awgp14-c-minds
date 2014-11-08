682f56d42b1d97cad8d629b691dbaa0db5bd89c5
Ryan Poole <rmp135@gmail.com>
2014-11-08 18:03:51 +0000

    Tidy up SFMLGraphicsDriver class.

--- SFMLLibrary/Drivers/SFMLGraphicsDriver.cs
+++ SFMLLibrary/Drivers/SFMLGraphicsDriver.cs
@@ -1,24 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Common;
 using Common.Interfaces;
-using Common;
-using SFML.Window;
 using SFML.Graphics;
-using OpenTK;
+using SFML.Window;
+using System;
+using System.Collections.Generic;
 
 namespace SFMLLibrary.Drivers
 {
     public class SFMLGraphicsDriver : IRenderDriver
     {
-        RenderWindow _window;
-        Dictionary<string, Sprite> _spriteMaps;
-        Dictionary<string, Text> _textMaps;
-        Font _font;
+        private RenderWindow _window;
+        private Dictionary<string, Sprite> _spriteMaps;
+        private Dictionary<string, Text> _textMaps;
+        private Font _font;
 
-        void onclose(object sender, EventArgs args) {
+        private void onclose(object sender, EventArgs args) {
             Environment.Exit(0);
         }
 
@@ -26,14 +22,14 @@ namespace SFMLLibrary.Drivers
             ContextSettings contextSettings = new ContextSettings();
             contextSettings.DepthBits = 32;
             _window = new RenderWindow(new VideoMode(640, 480), "Game", Styles.Titlebar | Styles.Close, contextSettings);
-            _spriteMaps = new Dictionary<string,Sprite>();
+            _spriteMaps = new Dictionary<string, Sprite>();
             _textMaps = new Dictionary<string, Text>();
             _font = new Font(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Fonts) + "/arial.ttf");
             // Make it the active window for OpenGL calls
             _window.SetVerticalSyncEnabled(true);
             _window.SetActive();
-           // Toolkit.Init();
-           _window.Closed += new EventHandler(onclose);
+            // Toolkit.Init();
+            _window.Closed += new EventHandler(onclose);
         }
 
         private Sprite CacheSprite(string spriteName) {
@@ -43,9 +39,9 @@ namespace SFMLLibrary.Drivers
         }
 
         private Text CacheText(string s) {
-           Text text = new Text(s, _font, 12);
-           _textMaps.Add(s, text);
-           return text;
+            Text text = new Text(s, _font, 12);
+            _textMaps.Add(s, text);
+            return text;
         }
 
         public void DrawSprite(string spriteName, Vector position) {
@@ -64,7 +60,7 @@ namespace SFMLLibrary.Drivers
                 text = CacheText(textToDraw);
             }
             text.Position = new Vector2f(pos.X, pos.Y);
-            text.Origin = new Vector2f(0.0f,0.0f);
+            text.Origin = new Vector2f(0.0f, 0.0f);
             text.Color = Color.Black;
             _window.Draw(text);
         }
@@ -72,8 +68,6 @@ namespace SFMLLibrary.Drivers
         public void PreRender() {
             _window.Clear(new Color((byte)50, (byte)50, (byte)180));
         }
-
-
 
         public void PostRender() {
             _window.DispatchEvents();
