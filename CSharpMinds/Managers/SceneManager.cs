147ec3ddb993043e8af1419ffd6933d7f080431c
Ryan Poole <rmp135@gmail.com>
2014-11-14 12:08:02 +0000

    SceneManager
    Added a static scene manager to managing scene transitions.

---
+++ CSharpMinds/Managers/SceneManager.cs
@@ -0,0 +1,34 @@
+﻿using CSharpMinds.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpMinds.Managers
+{
+    public static class SceneManager
+    {
+        static Dictionary<string, Scene> _scenes;
+        static Scene _currentScene;
+        public static void TransitionToScene(Scene nextScene) {
+            _currentScene = nextScene;
+        }
+
+        public static void AddGameObjectToScene(GameObject go) {
+            _currentScene.AddGameObject(go);
+        }
+
+        public static void RemoveGameObjectFromScene(GameObject go) {
+            _currentScene.RemoveGameObject(go);
+        }
+
+        public static void Update(GameTime gameTime) {
+            _currentScene.Update(gameTime);
+        }
+
+        public static void Render() {
+            _currentScene.Draw();
+        }
+    }
+}
