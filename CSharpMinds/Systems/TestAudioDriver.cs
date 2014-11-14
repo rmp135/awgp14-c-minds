c1c57fa35587e5ad39b5f032c662a581188985cd
Ryan Poole <rmp135@gmail.com>
2014-11-14 11:54:46 +0000

    Moved audio driver.
    The IAudioDriver is now a common interface for future implementation.

--- CSharpMinds/Systems/TestAudioDriver.cs
+++ CSharpMinds/Systems/TestAudioDriver.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Common.Interfaces;
 
 namespace CSharpMinds.Systems {
     public class TestAudioDriver : IAudioDriver {
