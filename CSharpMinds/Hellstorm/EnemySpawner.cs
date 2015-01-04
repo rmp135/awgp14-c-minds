908b6715de9d1af0d99e6c1df962ed012952beaf
Ryan Poole <rmp135@gmail.com>
2015-01-04 19:34:31 +0000

    Added Hellstorm game assets.

---
+++ CSharpMinds/Hellstorm/EnemySpawner.cs
@@ -0,0 +1,36 @@
+﻿using Common.Interfaces;
+using CSharpMinds.Components;
+using CSharpMinds.Factories;
+using CSharpMinds.Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpMinds.Hellstorm
+{
+    class EnemySpawner : Component, IUpdatable
+    {
+        private string[] _enemies;
+        public EnemySpawner()
+        {
+            _enemies = new string[] { "enemyBlack1","enemyBlack2","enemyBlack3","enemyBlack4","enemyBlack5" };
+        }
+        public void Update(Common.GameTime gameTime)
+        {
+            Random r = new Random();
+            if (gameTime.TotalTime % 1000 < 15)
+            {
+                SceneManager.AddGameObjectToScene(GameObjectFactory.Build("enemy", new List<Interfaces.IComponent>() {
+                new TransformComponent(new Common.Vector(700, r.Next(400))),
+                new PhysicsComponent(),
+                new EnemyLogic(),
+                new BoxColliderComponent(85, 95),
+                new SpriteRenderComponent("Hellstorm\\Resources\\" + _enemies[r.Next(_enemies.Length)] + ".png")
+            }));
+            }
+
+        }
+    }
+}
---
+++ CSharpMinds/Hellstorm/HellstormScene.cs
@@ -0,0 +1,38 @@
+﻿using CSharpMinds.Factories;
+using CSharpMinds.Interfaces;
+using CSharpMinds.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSharpMinds.Systems;
+using CSharpMinds.Managers;
+
+namespace CSharpMinds.Hellstorm
+{
+    public class HellstormScene : GameScene
+    {
+        public HellstormScene() : base()
+        {
+            //GameObject _player = GameObjectFactory.Build("player",new List<IComponent>() {
+            //    new TransformComponent(),
+            //    new SpriteRenderComponent("Hellstorm\\Resources\\playerShip1_red.png"),
+            //    new HellstormControlComponent(),
+            //    new PhysicsComponent(),
+            //    new BoxColliderComponent(75, 99)
+            //});
+            AddGameObject(GameObjectFactory.Build("background", new List<IComponent>() {
+                new TransformComponent(),
+                new SpriteRenderComponent("Hellstorm\\Resources\\darkPurple.png")
+            }));
+
+            GameObject _player = GameObjectFactory.BuildFromXML("Hellstorm\\Resources\\player.xml");
+            GameObject _health = GameObjectFactory.Build("health", new List<IComponent>() { new HealthComponent() });
+            AddGameObject(_player);
+            AddGameObject(_health);
+            AddGameObject(GameObjectFactory.Build("spawner", new List<IComponent>() { new EnemySpawner() }));
+            AddGameObject(GameObjectFactory.Build("powerupspawner", new List<IComponent>() { new PowerupSpawner() }));
+        }
+    }
+}
